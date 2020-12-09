d1fab3bc5c3099aef278137751a0ba95c51321e3
wickdchromosome <capsule.edu@gmail.com>
2020-12-08 18:22:56 -0700

    Fixed issue with bedtools version

--- annotate.sh
+++ annotate.sh
@@ -3,14 +3,23 @@ annotate_bed=$2
 outname=$3
 
 #save the header, add gene name to it
-filehead=$(head -n 1 ${infile})$'\t'name
+filehead=$(head -n 1 ${infile} | sed 's/position/position1\tposition2/g')$'\t'name
 
+#cut the header
+tail -n +2 ${infile} > ${outname}_nohead.temp 
+
+echo "Adding new pos columns"
+#add the pos-1 column
+paste -d$'\t' <(awk -F '\t' '{ print $1,$2-1,$2}' ${outname}_nohead.temp | sed 's/ /\t/g' ) <(cut -d$'\t' -f3- ${outname}_nohead.temp) > ${outname}_addedpos.temp
+
+echo "Doing intersection using bedtools"
 #intersect based on gene intervals
-bedtools intersect -loj -a <(tail -n +2 ${infile}) -b ${annotate_bed} > ${outname}.temp
+bedtools intersect -loj -a ${outname}_addedpos.temp -b ${annotate_bed} > ${outname}.temp
 
+echo "Writing final file"
 #cut out all the unnecessary shit
 echo ${filehead} | sed 's/ /\t/g' > ${outname}
-cat ${outname}.temp | cut -f-8,12 >> ${outname}
+cat ${outname}.temp | cut -f-9,13 >> ${outname}
 
-rm ${outname}.temp
+#rm ${outname}.temp
 
